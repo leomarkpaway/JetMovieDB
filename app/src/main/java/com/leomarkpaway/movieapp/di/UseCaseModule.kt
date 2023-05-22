785d7c319fe731ee5b9b85e5cbb2cfb4f56eb7fd
leoMarkDev <leomarkpaway@gmail.com>
2023-05-22 14:30:39 +0800

    :sparkles: Add use case provider

--- app/src/main/java/com/leomarkpaway/movieapp/di/UseCaseModule.kt
+++ app/src/main/java/com/leomarkpaway/movieapp/di/UseCaseModule.kt
@@ -1,13 +1,24 @@
 package com.leomarkpaway.movieapp.di
 
+import com.leomarkpaway.movieapp.domain.repository.MovieRepository
+import com.leomarkpaway.movieapp.domain.usecase.AddMovieUseCase
+import com.leomarkpaway.movieapp.domain.usecase.GetAllMovieUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
 
-    //TODO add usecase provider here
+    @Provides
+    @Singleton
+    fun provideGetAllMovieUseCase(movieRepository: MovieRepository) = GetAllMovieUseCase(movieRepository)
+
+    @Provides
+    @Singleton
+    fun provideAddMovieUseCase(movieRepository: MovieRepository) = AddMovieUseCase(movieRepository)
 
 }
