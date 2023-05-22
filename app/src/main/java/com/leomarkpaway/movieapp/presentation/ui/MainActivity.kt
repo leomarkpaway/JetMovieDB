87c4a84bce90d3c3235aacad27cb4843219d3833
leoMarkDev <leomarkpaway@gmail.com>
2023-05-22 15:12:12 +0800

    :hammer: Use base class activity on main activity

--- app/src/main/java/com/leomarkpaway/movieapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/movieapp/presentation/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.leomarkpaway.movieapp.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.activity.viewModels
 import com.leomarkpaway.movieapp.R
+import com.leomarkpaway.movieapp.common.base.BaseActivity
+import com.leomarkpaway.movieapp.databinding.ActivityMainBinding
+import com.leomarkpaway.movieapp.presentation.viewmodel.MainActivityViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>() {
+    override val layoutId: Int = R.layout.activity_main
+    override val viewModel: MainActivityViewModel by viewModels()
+
+    override fun initViews() {
+        super.initViews()
+        //TODO initial view here
     }
+
+    override fun subscribe() {
+        super.subscribe()
+        //TODO observe data here
+    }
+
 }
