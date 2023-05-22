a8e0abca6b0d853c70e6a1b5f86af0e4843f2541
leoMarkDev <leomarkpaway@gmail.com>
2023-05-22 15:11:06 +0800

    :sparkles: Create main view model

---
+++ app/src/main/java/com/leomarkpaway/movieapp/presentation/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.leomarkpaway.movieapp.presentation.viewmodel
+
+import com.leomarkpaway.movieapp.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor() : BaseViewModel() {}
