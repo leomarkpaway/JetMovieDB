03bc118355aea06873e5dfd04e9e7ebb8f12c77d
leoMarkDev <leomarkpaway@gmail.com>
2023-05-22 13:04:41 +0800

    :sparkles: Create base classes

---
+++ app/src/main/java/com/leomarkpaway/movieapp/common/base/BaseActivity.kt
@@ -0,0 +1,36 @@
+package com.leomarkpaway.movieapp.common.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : BaseViewModel, B : ViewDataBinding> : AppCompatActivity() {
+
+    abstract val layoutId: Int
+    abstract val viewModel: T
+    lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        initViews()
+        subscribe()
+    }
+
+    fun hideSystemUI() {
+        supportActionBar?.hide()
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    open fun initViews() {}
+
+    open fun subscribe() {}
+}
