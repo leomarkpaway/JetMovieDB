7d5661092151e8d0513a0a7c7fb2dcc4f4ba732a
leoMarkDev <leomarkpaway@gmail.com>
2023-05-23 10:43:47 +0800

    :sparkles: Implement set and check database state

--- app/src/main/java/com/leomarkpaway/movieapp/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/leomarkpaway/movieapp/data/repository/MovieRepositoryImpl.kt
@@ -2,10 +2,12 @@ package com.leomarkpaway.movieapp.data.repository
 
 import com.leomarkpaway.movieapp.data.source.local.database.AppDatabase
 import com.leomarkpaway.movieapp.data.source.local.entity.Movie
+import com.leomarkpaway.movieapp.data.source.preference.PreferenceManager
 import com.leomarkpaway.movieapp.domain.repository.MovieRepository
 
 class MovieRepositoryImpl(
-    private val appDatabase: AppDatabase
+    private val appDatabase: AppDatabase,
+    private val preference: PreferenceManager
 ) : MovieRepository {
     override suspend fun addMovie(movie: List<Movie>) {
         appDatabase.movieDao().insertAll(movie)
@@ -17,5 +19,13 @@ class MovieRepositoryImpl(
 
     override suspend fun sortByDate(): List<Movie> {
         return appDatabase.movieDao().sortByDate()
+    }
+
+    override suspend fun setDataBaseState(isDataBaseEmpty: Boolean) {
+        preference.saveBoolean(preferenceKey = "preference_key", value = isDataBaseEmpty)
+    }
+
+    override suspend fun checkDataBaseState(): Boolean? {
+        return preference.getBoolean(preferenceKey = "preference_key")
     }
 }
