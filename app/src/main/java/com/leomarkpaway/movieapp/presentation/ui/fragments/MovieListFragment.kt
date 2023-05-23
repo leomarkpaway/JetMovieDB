3cf0232ea741314ab59320a5b05ecf4bc7392e48
leoMarkDev <leomarkpaway@gmail.com>
2023-05-23 12:31:55 +0800

    :sparkles: Create movie list fragment

---
+++ app/src/main/java/com/leomarkpaway/movieapp/presentation/ui/fragments/MovieListFragment.kt
@@ -0,0 +1,60 @@
+package com.leomarkpaway.movieapp.presentation.ui.fragments
+
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.leomarkpaway.movieapp.R
+import com.leomarkpaway.movieapp.common.base.BaseFragment
+import com.leomarkpaway.movieapp.common.util.collectLatestData
+import com.leomarkpaway.movieapp.data.source.local.entity.Movie
+import com.leomarkpaway.movieapp.databinding.FragmentMovieListBinding
+import com.leomarkpaway.movieapp.presentation.ui.adapter.MovieAdapter
+import com.leomarkpaway.movieapp.presentation.viewmodel.SharedViewModel
+
+class MovieListFragment : BaseFragment<FragmentMovieListBinding, SharedViewModel>() {
+
+    private var isSortByDate = false
+
+    override fun initViews() {
+        super.initViews()
+        viewModel.setDataBaseState(isDataBaseEmpty = true)
+        viewModel.getDataBaseState()
+        onClickSort()
+    }
+
+    override fun subscribe() {
+        super.subscribe()
+        with(viewModel) {
+            allMovie.collectLatestData(lifecycleScope) { movieList ->
+                if (!movieList.isNullOrEmpty()) {
+                    setupMovieAdapter(movieList)
+                    setDataBaseState(isDataBaseEmpty = false)
+                }
+            }
+            isSortByDate.collectLatestData(lifecycleScope) { isSortByDate ->
+                this@MovieListFragment.isSortByDate = isSortByDate
+                if (isSortByDate) sortByDate() else getAllMovies()
+            }
+            isDataBaseEmpty.collectLatestData(lifecycleScope) {  state ->
+                if (state == true) addMovie()
+            }
+        }
+    }
+
+    private fun onClickSort() = with(viewModel){
+        binding.tvSort.setOnClickListener {
+            if (!this@MovieListFragment.isSortByDate) updateIsSortByDate(true)
+            else updateIsSortByDate(false)
+        }
+    }
+
+    private fun setupMovieAdapter(movieList : List<Movie>) = with(binding) {
+        rvMovies.adapter = MovieAdapter(movieList) { id -> onClick(id) }
+        rvMovies.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun onClick(id: Long?) {
+        findNavController().navigate(R.id.action_movieDetail)
+    }
+
+}
