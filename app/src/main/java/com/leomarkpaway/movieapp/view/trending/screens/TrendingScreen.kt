e49bc3eeb0f4076c2a8191e133c94ef04b2a331c
leomarkpaway <leomarkpaway@gmail.com>
2024-09-11 15:52:36 +0800

    :bug: Hot fix loading ui issue

--- app/src/main/java/com/leomarkpaway/movieapp/view/trending/screens/TrendingScreen.kt
+++ app/src/main/java/com/leomarkpaway/movieapp/view/trending/screens/TrendingScreen.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leomarkpaway.movieapp.intents.HomeInteractionEvents
@@ -39,9 +37,10 @@ fun TrendingScreen(moviesHomeInteractionEvents: (HomeInteractionEvents) -> Unit)
             .horizontalGradientBackground(surfaceGradient)
             .verticalScroll(rememberScrollState())
     ) {
-        if (showLoading.value) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        }
+        // TODO Fix loading issue
+//        if (showLoading.value) {
+//            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+//        }
 
         listOfSections.forEach {
             DynamicSection(it, viewModel, showLoading, moviesHomeInteractionEvents)
